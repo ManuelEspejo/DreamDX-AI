4c67bf3b02744db79a9be9959f08bdb5f128d44d
Manuel <manuelesph@gmail.com>
2024-08-27 09:59:02 +0200

    create simple home page

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DreamDX",
+    page_icon=":new_moon_with_face:"
+)
+st.write("# Welcome to DreamDX AI! :night_with_stars:")
+st.markdown(
+    """
+    DreamDX AI is an AI-powered application that allows you to revisit and take control of your dreams.
+
+    ### Documentation
+    - Github repo: [DreamDX AI](https://github.com/ManuelEspejo/DreamDX-AI)
+"""
+)
